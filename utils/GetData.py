75d78bee8a35a331e08eca7e63b206bb4c99aa9c
Daniel Reyes Barrera <danieldhatsct@gmail.com>
2021-12-16 10:09:08 -0600

    mini update

--- utils/GetData.py
+++ utils/GetData.py
@@ -1,9 +1,29 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split, KFold, cross_validate, RandomizedSearchCV, cross_val_score
-from sklearn.metrics import mean_absolute_error
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder
 
-def get_data(PATH):
+
+class Data:
+
+    def __init__(self):
+        self.bad_features = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu']
     
-    df = pd.read_csv(PATH)
+    def get_data(self, df):
+        # Drop bad features
+        df.drop(self.bad_features, axis=1, inplace=True)
+
+        # Imputer mising values
+        df_object = df.select_dtypes('object')
+        df_num  = df.select_dtypes('number')
+
+        numerical_imputer = SimpleImputer(strategy='constant')
+        object_imputer = SimpleImputer(strategy='most_frequent')
+        ordinal = OrdinalEncoder(handle_unknown='error')
+
+        df_num_imp = pd.DataFrame(numerical_imputer.fit_transform(df_num), columns= df_num.columns)
+        df_object_imp = pd.DataFrame(object_imputer.fit_transform(df_object), columns= df_object.columns)
+        df_object_ord = pd.DataFrame(ordinal.fit_transform(df_object_imp), columns=df_object_imp.columns)
+
+        df_ = pd.concat([df_num_imp,df_object_ord], axis=1)
+
+        return df_
