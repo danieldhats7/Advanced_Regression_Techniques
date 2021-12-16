5af0da99a35f395d3b01bf2921f1582c270b9c83
Daniel Reyes Barrera <danieldhatsct@gmail.com>
2021-12-16 11:16:03 -0600

    0.17367

--- utils/GetData.py
+++ utils/GetData.py
@@ -7,8 +7,11 @@ class Data:
 
     def __init__(self):
         self.bad_features = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu']
+        self.numerical_imputer = SimpleImputer(strategy='constant')
+        self.object_imputer = SimpleImputer(strategy='most_frequent')
+        self.ordinal = OrdinalEncoder(handle_unknown='error')
     
-    def get_data(self, df):
+    def get_data(self, df, fit=True):
         # Drop bad features
         df.drop(self.bad_features, axis=1, inplace=True)
 
@@ -16,13 +19,14 @@ class Data:
         df_object = df.select_dtypes('object')
         df_num  = df.select_dtypes('number')
 
-        numerical_imputer = SimpleImputer(strategy='constant')
-        object_imputer = SimpleImputer(strategy='most_frequent')
-        ordinal = OrdinalEncoder(handle_unknown='error')
-
-        df_num_imp = pd.DataFrame(numerical_imputer.fit_transform(df_num), columns= df_num.columns)
-        df_object_imp = pd.DataFrame(object_imputer.fit_transform(df_object), columns= df_object.columns)
-        df_object_ord = pd.DataFrame(ordinal.fit_transform(df_object_imp), columns=df_object_imp.columns)
+        if fit == True:
+            df_num_imp = pd.DataFrame(self.numerical_imputer.fit_transform(df_num), columns= df_num.columns)
+            df_object_imp = pd.DataFrame(self.object_imputer.fit_transform(df_object), columns= df_object.columns)
+            df_object_ord = pd.DataFrame(self.ordinal.fit_transform(df_object_imp), columns=df_object_imp.columns)
+        else:
+            df_num_imp = pd.DataFrame(self.numerical_imputer.transform(df_num), columns= df_num.columns)
+            df_object_imp = pd.DataFrame(self.object_imputer.transform(df_object), columns= df_object.columns)
+            df_object_ord = pd.DataFrame(self.ordinal.transform(df_object_imp), columns=df_object_imp.columns)
 
         df_ = pd.concat([df_num_imp,df_object_ord], axis=1)
 
